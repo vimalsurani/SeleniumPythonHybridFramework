33d2a17b35e11dc855f87951e540f5bc7d834cea
ViMS <vimal.surani@gmail.com>
2024-06-23 15:28:23 +0530

    Generating Allure Reports And Taking Screenshot on failure

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,26 @@
+import allure
 import pytest
+from allure_commons.types import AttachmentType
 from selenium import webdriver
 
 from utilities import ReadConfigurations
+
+
+@pytest.fixture()
+def log_on_failure(request):
+    yield
+    item = request.node
+    if item.rep_call.failed:
+        allure.attach(driver.get_screenshot_as_png(), name="failed_test",
+                      attachment_type=AttachmentType.PNG)
+
+
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep
 
 
 @pytest.fixture()
