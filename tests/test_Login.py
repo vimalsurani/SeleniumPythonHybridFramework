a622fa73075a9e96fe8033a4d856bd70d2627841
ViMS <vimal.surani@gmail.com>
2024-06-23 13:27:55 +0530

    Moving duplicate method to BaseTest

--- tests/test_Login.py
+++ tests/test_Login.py
@@ -14,13 +14,13 @@ class TestLogin(BaseTest):
     def test_with_valid_credentials(self):
         home_page = HomePage(self.driver)
         login_page = home_page.navigate_to_login_page()
-        account_page = login_page.login_to_application("vimalpatel7449@gmail.com","Abc123456789")
+        account_page = login_page.login_to_application("vimalpatel7449@gmail.com", "Abc123456789")
         assert account_page.display_status_of_edit_your_account_information_option()
 
     def test_with_invalid_email_valid_password(self):
         home_page = HomePage(self.driver)
         login_page = home_page.navigate_to_login_page()
-        login_page.login_to_application(self.generate_email_with_timestamp(),"Abc123456789")
+        login_page.login_to_application(self.generate_email_with_timestamp(), "Abc123456789")
         expected_warning_message = "Warning: No match for E-Mail Address and/or Password."
         assert login_page.retrieve_warning_message().__eq__(
             expected_warning_message)
@@ -28,7 +28,7 @@ class TestLogin(BaseTest):
     def test_with_valid_email_invalid_password(self):
         home_page = HomePage(self.driver)
         login_page = home_page.navigate_to_login_page()
-        login_page.login_to_application("vimalpatel7449@gmail.com","123456789")
+        login_page.login_to_application("vimalpatel7449@gmail.com", "123456789")
         expected_warning_message = "Warning: No match for E-Mail Address and/or Password."
         assert login_page.retrieve_warning_message().__eq__(
             expected_warning_message)
@@ -36,11 +36,7 @@ class TestLogin(BaseTest):
     def test_without_entering_any_credentials(self):
         home_page = HomePage(self.driver)
         login_page = home_page.navigate_to_login_page()
-        login_page.login_to_application("","")
+        login_page.login_to_application("", "")
         expected_warning_message = "Warning: No match for E-Mail Address and/or Password."
         assert login_page.retrieve_warning_message().__eq__(
             expected_warning_message)
-
-    def generate_email_with_timestamp(self):
-        time_stamp = datetime.now().strftime("%Y_%m_%d %H_%M_%S")
-        return "vimal" + time_stamp + "@gmail.com"
